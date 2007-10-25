cc60ee6c50315984c4330d33f6a287f6088c1e6a
Andrus Adamchik <aadamchik@apache.org>
2007-10-25 20:10:18 +0000

    CAY-903 Support primitive types in DataObjects (oops , my temp additions were breaking ALL unit tests...)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@588332 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c35c90d6a34d1fe9df92cffeb8b21ecaa584b892

---
+++ framework/cayenne-jdk1.4-unpublished/src/test/java/org/apache/art/PrimitivesTestEntity.java
@@ -0,0 +1,42 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*****************************************************************  *   Licensed to the Apache Software Foundation (ASF) under one  *  or more contributor license agreements.  See the NOTICE file  *  distributed with this work for additional information  *  regarding copyright ownership.  The ASF licenses this file  *  to you under the Apache License, Version 2.0 (the  *  "License"); you may not use this file except in compliance  *  with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing,  *  software distributed under the License is distributed on an  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  *  KIND, either express or implied.  See the License for the  *  specific language governing permissions and limitations  *  under the License.  ****************************************************************/
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|art
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|cayenne
+operator|.
+name|CayenneDataObject
+import|;
+end_import
+
+begin_comment
+comment|// TODO: andrus, 10/25/2007 a placeholder until CAY-904 is fixed and CAY-903 can be fully tested
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|PrimitivesTestEntity
+extends|extends
+name|CayenneDataObject
+block|{  }
+end_class
+
+end_unit
+
