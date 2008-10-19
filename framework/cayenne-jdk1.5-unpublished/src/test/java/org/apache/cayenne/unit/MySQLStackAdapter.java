0da202fefede12a0f0c3c1d40b1695ee11e4dfe1
Andrus Adamchik <aadamchik@apache.org>
2008-10-19 11:44:50 +0000

    CAY-1125 MySQL SMALLINT UNSIGNED is mapped as java.lang.Short, should be java.lang.Integer (can't reproduce)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@705982 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6e1c48ef70d4aca63d2e02092a31bfcf31cbea70

--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/unit/MySQLStackAdapter.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/unit/MySQLStackAdapter.java
@@ -252,15 +252,6 @@ argument_list|,
 literal|"create-out-sp.sql"
 argument_list|)
 expr_stmt|;
-name|executeDDL
-argument_list|(
-name|con
-argument_list|,
-literal|"mysql"
-argument_list|,
-literal|"create-smallint-unsigned.sql"
-argument_list|)
-expr_stmt|;
 block|}
 block|}
 annotation|@
