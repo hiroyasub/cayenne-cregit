dcbe5239a5f57f8df00036cb1e464b859ac4fc89
Andrus Adamchik <aadamchik@apache.org>
2009-03-11 11:56:50 +0000

    CAY-1164 Implement EJBQL query support in the Modeler
    patch by Olga Tkachova implementing our own text pane with syntax hihglighting
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@752430 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 392d59702016741af3096c13063df7b0fd0bbc57

---
+++ framework/cayenne-modeler/src/main/java/org/apache/cayenne/swing/components/textpane/style/TextPaneStyleTypes.java
@@ -0,0 +1,49 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|cayenne
+operator|.
+name|swing
+operator|.
+name|components
+operator|.
+name|textpane
+operator|.
+name|style
+package|;
+end_package
+
+begin_enum
+specifier|public
+enum|enum
+name|TextPaneStyleTypes
+block|{
+name|KEYWORDS
+block|,
+comment|// Language keywords
+name|KEYWORDS2
+block|,
+comment|// Language keywords with style2
+name|OPERATORS
+block|,
+comment|// Language operators
+name|NUMBER
+block|,
+comment|// numbers in various formats
+name|STRING
+block|,
+comment|// String
+name|COMMENT
+block|,
+comment|// comments
+name|TYPE
+comment|// Types, usually not keywords, but supported by the language
+block|}
+end_enum
+
+end_unit
+
