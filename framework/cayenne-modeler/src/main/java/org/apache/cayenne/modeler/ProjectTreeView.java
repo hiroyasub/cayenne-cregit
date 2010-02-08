06f6850e066e3915e69cba42fcddece11df0bd85
Olga Tkachova <oltka@apache.org>
2010-02-08 15:59:29 +0000

    CAY-1339 Remove org.apache.cayenne.project package from runtime
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@907707 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b97267a4901acab6b1bb0140cd6f65d072238d4a

--- framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/ProjectTreeView.java
+++ framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/ProjectTreeView.java
@@ -3342,11 +3342,6 @@ name|isNameChange
 argument_list|()
 condition|)
 block|{
-name|mediator
-operator|.
-name|updateEntityResolver
-argument_list|()
-expr_stmt|;
 name|positionNode
 argument_list|(
 name|path
