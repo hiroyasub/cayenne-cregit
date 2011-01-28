cd129805b7aaa07b47e036a029ab7b3ad3d3aef4
Andrus Adamchik <aadamchik@apache.org>
2011-01-28 13:27:19 +0000

    CAY-1532 Import cayenne-lifecycle from sandbox
    initial copy of cayenne-mixin
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1064663 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c76e8950a519b7dd94fc0ce4bf174767d4d68679

---
+++ framework/cayenne-lifecycle/src/main/java/org/apache/cayenne/mixin/audit/AuditableOperation.java
@@ -0,0 +1,34 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|cayenne
+operator|.
+name|mixin
+operator|.
+name|audit
+package|;
+end_package
+
+begin_comment
+comment|/**  * An enum of auditable operations.  */
+end_comment
+
+begin_enum
+specifier|public
+enum|enum
+name|AuditableOperation
+block|{
+name|INSERT
+block|,
+name|UPDATE
+block|,
+name|DELETE
+block|; }
+end_enum
+
+end_unit
+
