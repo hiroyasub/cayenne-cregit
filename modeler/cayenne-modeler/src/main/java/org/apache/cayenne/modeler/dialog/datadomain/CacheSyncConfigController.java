d799c12352499bd29473cbf128cf720bcb895c7a
Dzmitry Kazimirchyk <dkazimirchyk@apache.org>
2011-07-04 12:53:01 +0000

    CAY-1461 CayenneModeler: remove ScopeMVC dependencies
    removed scopemvc dependency from the main pom
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1142645 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5515390e47cf1fcb1b7c5bb5226ddf09b8b927f3

--- modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/dialog/datadomain/CacheSyncConfigController.java
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/dialog/datadomain/CacheSyncConfigController.java
@@ -214,7 +214,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A controller for CacheSyncConfigDialog and its subviews. Builds a model out of a  * DataDomain properties map, and on save updates DataDomain properties with configuration  * changes made by the user. This controller manages one main dialog view, and its  * subviews organized using CardLayout.  *   */
+comment|/**  * A controller for CacheSyncConfigDialog and its subviews. This controller   * manages one main dialog view, and its subviews organized using CardLayout.  *   */
 end_comment
 
 begin_class
