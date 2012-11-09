2fc15b83ac511756606cbdc47d3036a51940b903
Andrus Adamchik <aadamchik@apache.org>
2012-11-09 20:05:42 +0000

    CAY-1766 Deprecating DataPort ant task
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1407613 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 812e4ba9fe56acd0c2d759be0eaa29258948b83d

--- framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/access/DataPortDelegate.java
+++ framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/access/DataPortDelegate.java
@@ -54,10 +54,12 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Interface for callback and delegate methods allowing implementing classes to control  * various aspects of data porting via DataPort. DataPort instance will invoke appropriate  * delegate methods during different stages of porting process.  *   * @since 1.2: Prior to 1.2 DataPort classes were a part of cayenne-examples package.  */
+comment|/**  * Interface for callback and delegate methods allowing implementing classes to control  * various aspects of data porting via DataPort. DataPort instance will invoke appropriate  * delegate methods during different stages of porting process.  *   * @since 1.2: Prior to 1.2 DataPort classes were a part of cayenne-examples package.  * @deprecated since 3.2  */
 end_comment
 
 begin_interface
+annotation|@
+name|Deprecated
 specifier|public
 interface|interface
 name|DataPortDelegate
