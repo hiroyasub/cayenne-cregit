536beb79cb1288d0d243c8d891d64cfbe2006ada
Andrus Adamchik <aadamchik@apache.org>
2012-11-09 13:48:05 +0000

    CAY-1759 cdbimport improvements: add 'catalog', rename 'schemaName' to 'schema'
    * refactoring logger use
    * reformatting code
    * adding 'catalog' parameter (yet unused)
    * renaming schemaName to schema
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1407449 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 82d30a73c675d28a3467e057b2d68d180b694642

--- framework/maven-cayenne-plugin/src/main/java/org/apache/cayenne/tools/DbImporterMojo.java
+++ framework/maven-cayenne-plugin/src/main/java/org/apache/cayenne/tools/DbImporterMojo.java
@@ -303,20 +303,6 @@ name|org
 operator|.
 name|apache
 operator|.
-name|commons
-operator|.
-name|logging
-operator|.
-name|Log
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
 name|maven
 operator|.
 name|plugin
@@ -376,75 +362,80 @@ name|DbImporterMojo
 extends|extends
 name|AbstractMojo
 block|{
-comment|/** 	 * DataMap XML file to use as a base for DB importing. 	 *  	 * @parameter expression="${cdbimport.map}" 	 * @required 	 */
+comment|/**      * DataMap XML file to use as a base for DB importing.      *       * @parameter expression="${cdbimport.map}"      * @required      */
 specifier|private
 name|File
 name|map
 decl_stmt|;
-comment|/** 	 * Indicates whether existing DB and object entities should be overwritten. 	 * This is an all-or-nothing setting. If you need finer granularity, please 	 * use the Cayenne Modeler. 	 *  	 * Default is<code>true</code>. 	 *  	 * @parameter expression="${cdbimport.overwriteExisting}" 	 *            default-value="true" 	 */
+comment|/**      * Indicates whether existing DB and object entities should be overwritten.      * This is an all-or-nothing setting. If you need finer granularity, please      * use the Cayenne Modeler.      *       * Default is<code>true</code>.      *       * @parameter expression="${cdbimport.overwriteExisting}"      *            default-value="true"      */
 specifier|private
 name|boolean
 name|overwriteExisting
 decl_stmt|;
-comment|/** 	 * DB schema to use for DB importing. 	 *  	 * @parameter expression="${cdbimport.schemaName}" 	 */
+comment|/**      * DB schema to use for DB importing.      *       * @parameter expression="${cdbimport.schemaName}"      * @deprecated since 3.2 renamed to "schema"      */
 specifier|private
 name|String
 name|schemaName
 decl_stmt|;
-comment|/** 	 * Pattern for tables to import from DB. 	 *  	 * The default is to match against all tables. 	 *  	 * @parameter expression="${cdbimport.tablePattern}" 	 */
+comment|/**      * DB schema to use for DB importing.      *       * @parameter expression="${cdbimport.catalog}"      * @since 3.2      */
+specifier|private
+name|String
+name|catalog
+decl_stmt|;
+comment|/**      * DB schema to use for DB importing.      *       * @parameter expression="${cdbimport.schema}"      * @since 3.2      */
+specifier|private
+name|String
+name|schema
+decl_stmt|;
+comment|/**      * Pattern for tables to import from DB.      *       * The default is to match against all tables.      *       * @parameter expression="${cdbimport.tablePattern}"      */
 specifier|private
 name|String
 name|tablePattern
 decl_stmt|;
-comment|/** 	 * Indicates whether stored procedures should be imported. 	 *  	 * Default is<code>false</code>. 	 *  	 * @parameter expression="${cdbimport.importProcedures}" 	 *            default-value="false" 	 */
+comment|/**      * Indicates whether stored procedures should be imported.      *       * Default is<code>false</code>.      *       * @parameter expression="${cdbimport.importProcedures}"      *            default-value="false"      */
 specifier|private
 name|boolean
 name|importProcedures
 decl_stmt|;
-comment|/** 	 * Pattern for stored procedures to import from DB. This is only meaningful 	 * if<code>importProcedures</code> is set to<code>true</code>. 	 *  	 * The default is to match against all stored procedures. 	 *  	 * @parameter expression="${cdbimport.procedurePattern}" 	 */
+comment|/**      * Pattern for stored procedures to import from DB. This is only meaningful      * if<code>importProcedures</code> is set to<code>true</code>.      *       * The default is to match against all stored procedures.      *       * @parameter expression="${cdbimport.procedurePattern}"      */
 specifier|private
 name|String
 name|procedurePattern
 decl_stmt|;
-comment|/** 	 * Indicates whether primary keys should be mapped as meaningful attributes 	 * in the object entities. 	 *  	 * Default is<code>false</code>. 	 *  	 * @parameter expression="${cdbimport.meaningfulPk}" default-value="false" 	 */
+comment|/**      * Indicates whether primary keys should be mapped as meaningful attributes      * in the object entities.      *       * Default is<code>false</code>.      *       * @parameter expression="${cdbimport.meaningfulPk}" default-value="false"      */
 specifier|private
 name|boolean
 name|meaningfulPk
 decl_stmt|;
-comment|/** 	 * Java class implementing org.apache.cayenne.map.naming.NamingStrategy. 	 * This is used to specify how ObjEntities will be mapped from the imported 	 * DB schema. 	 *  	 * The default is a basic naming strategy. 	 *  	 * @parameter expression="${cdbimport.namingStrategy}" 	 *            default-value="org.apache.cayenne.map.naming.SmartNamingStrategy" 	 */
+comment|/**      * Java class implementing org.apache.cayenne.map.naming.NamingStrategy.      * This is used to specify how ObjEntities will be mapped from the imported      * DB schema.      *       * The default is a basic naming strategy.      *       * @parameter expression="${cdbimport.namingStrategy}"      *            default-value="org.apache.cayenne.map.naming.SmartNamingStrategy"      */
 specifier|private
 name|String
 name|namingStrategy
 decl_stmt|;
-comment|/** 	 * Java class implementing org.apache.cayenne.dba.DbAdapter. While this 	 * attribute is optional (a generic JdbcAdapter is used if not set), it is 	 * highly recommended to specify correct target adapter. 	 *  	 * @parameter expression="${cdbimport.adapter}" 	 */
+comment|/**      * Java class implementing org.apache.cayenne.dba.DbAdapter. While this      * attribute is optional (a generic JdbcAdapter is used if not set), it is      * highly recommended to specify correct target adapter.      *       * @parameter expression="${cdbimport.adapter}"      */
 specifier|private
 name|String
 name|adapter
 decl_stmt|;
-comment|/** 	 * A class of JDBC driver to use for the target database. 	 *  	 * @parameter expression="${cdbimport.driver}" 	 * @required 	 */
+comment|/**      * A class of JDBC driver to use for the target database.      *       * @parameter expression="${cdbimport.driver}"      * @required      */
 specifier|private
 name|String
 name|driver
 decl_stmt|;
-comment|/** 	 * JDBC connection URL of a target database. 	 *  	 * @parameter expression="${cdbimport.url}" 	 * @required 	 */
+comment|/**      * JDBC connection URL of a target database.      *       * @parameter expression="${cdbimport.url}"      * @required      */
 specifier|private
 name|String
 name|url
 decl_stmt|;
-comment|/** 	 * Database user name. 	 *  	 * @parameter expression="${cdbimport.username}" 	 */
+comment|/**      * Database user name.      *       * @parameter expression="${cdbimport.username}"      */
 specifier|private
 name|String
 name|username
 decl_stmt|;
-comment|/** 	 * Database user password. 	 *  	 * @parameter expression="${cdbimport.password}" 	 */
+comment|/**      * Database user password.      *       * @parameter expression="${cdbimport.password}"      */
 specifier|private
 name|String
 name|password
-decl_stmt|;
-comment|/** 	 * Maven logger. 	 */
-specifier|private
-name|Log
-name|logger
 decl_stmt|;
 specifier|public
 name|void
@@ -455,6 +446,135 @@ name|MojoExecutionException
 throws|,
 name|MojoFailureException
 block|{
+name|getLog
+argument_list|()
+operator|.
+name|debug
+argument_list|(
+name|String
+operator|.
+name|format
+argument_list|(
+literal|"connection settings - [driver: %s, url: %s, username: %s, password: %s]"
+argument_list|,
+name|driver
+argument_list|,
+name|url
+argument_list|,
+name|username
+argument_list|,
+name|password
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|getLog
+argument_list|()
+operator|.
+name|info
+argument_list|(
+name|String
+operator|.
+name|format
+argument_list|(
+literal|"importer options - [map: %s, overwriteExisting: %s, schema: %s, tablePattern: %s, importProcedures: %s, procedurePattern: %s, meaningfulPk: %s, namingStrategy: %s]"
+argument_list|,
+name|map
+argument_list|,
+name|overwriteExisting
+argument_list|,
+name|schema
+argument_list|,
+name|tablePattern
+argument_list|,
+name|importProcedures
+argument_list|,
+name|procedurePattern
+argument_list|,
+name|meaningfulPk
+argument_list|,
+name|namingStrategy
+argument_list|)
+argument_list|)
+expr_stmt|;
+try|try
+block|{
+name|doExecute
+argument_list|()
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|Exception
+name|ex
+parameter_list|)
+block|{
+name|Throwable
+name|th
+init|=
+name|Util
+operator|.
+name|unwindException
+argument_list|(
+name|ex
+argument_list|)
+decl_stmt|;
+name|String
+name|message
+init|=
+literal|"Error importing database schema"
+decl_stmt|;
+if|if
+condition|(
+name|th
+operator|.
+name|getLocalizedMessage
+argument_list|()
+operator|!=
+literal|null
+condition|)
+block|{
+name|message
+operator|+=
+literal|": "
+operator|+
+name|th
+operator|.
+name|getLocalizedMessage
+argument_list|()
+expr_stmt|;
+block|}
+name|getLog
+argument_list|()
+operator|.
+name|error
+argument_list|(
+name|message
+argument_list|)
+expr_stmt|;
+throw|throw
+operator|new
+name|MojoExecutionException
+argument_list|(
+name|message
+argument_list|,
+name|th
+argument_list|)
+throw|;
+block|}
+block|}
+specifier|private
+name|void
+name|doExecute
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|String
+name|schema
+init|=
+name|getSchema
+argument_list|()
+decl_stmt|;
 name|Injector
 name|injector
 init|=
@@ -479,65 +599,6 @@ operator|.
 name|class
 argument_list|)
 decl_stmt|;
-name|logger
-operator|=
-operator|new
-name|MavenLogger
-argument_list|(
-name|this
-argument_list|)
-expr_stmt|;
-name|logger
-operator|.
-name|debug
-argument_list|(
-name|String
-operator|.
-name|format
-argument_list|(
-literal|"connection settings - [driver: %s, url: %s, username: %s, password: %s]"
-argument_list|,
-name|driver
-argument_list|,
-name|url
-argument_list|,
-name|username
-argument_list|,
-name|password
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|logger
-operator|.
-name|info
-argument_list|(
-name|String
-operator|.
-name|format
-argument_list|(
-literal|"importer options - [map: %s, overwriteExisting: %s, schemaName: %s, tablePattern: %s, importProcedures: %s, procedurePattern: %s, meaningfulPk: %s, namingStrategy: %s]"
-argument_list|,
-name|map
-argument_list|,
-name|overwriteExisting
-argument_list|,
-name|schemaName
-argument_list|,
-name|tablePattern
-argument_list|,
-name|importProcedures
-argument_list|,
-name|procedurePattern
-argument_list|,
-name|meaningfulPk
-argument_list|,
-name|namingStrategy
-argument_list|)
-argument_list|)
-expr_stmt|;
-try|try
-block|{
-specifier|final
 name|DbAdapter
 name|adapterInst
 init|=
@@ -647,7 +708,6 @@ operator|!=
 literal|null
 condition|)
 block|{
-specifier|final
 name|NamingStrategy
 name|namingStrategyInst
 init|=
@@ -692,7 +752,7 @@ name|loader
 operator|.
 name|loadDataMapFromDB
 argument_list|(
-name|schemaName
+name|schema
 argument_list|,
 name|tablePattern
 argument_list|,
@@ -727,7 +787,7 @@ name|loader
 operator|.
 name|loadProceduresFromDB
 argument_list|(
-name|schemaName
+name|schema
 argument_list|,
 name|procedurePattern
 argument_list|,
@@ -781,64 +841,36 @@ name|close
 argument_list|()
 expr_stmt|;
 block|}
-catch|catch
-parameter_list|(
-name|Exception
-name|ex
-parameter_list|)
-block|{
-name|Throwable
-name|th
-init|=
-name|Util
-operator|.
-name|unwindException
-argument_list|(
-name|ex
-argument_list|)
-decl_stmt|;
+specifier|private
 name|String
-name|message
-init|=
-literal|"Error importing database schema"
-decl_stmt|;
+name|getSchema
+parameter_list|()
+block|{
 if|if
 condition|(
-name|th
-operator|.
-name|getLocalizedMessage
-argument_list|()
+name|schemaName
 operator|!=
 literal|null
 condition|)
 block|{
-name|message
-operator|+=
-literal|": "
-operator|+
-name|th
-operator|.
-name|getLocalizedMessage
+name|getLog
 argument_list|()
-expr_stmt|;
-block|}
-name|logger
 operator|.
-name|error
+name|warn
 argument_list|(
-name|message
+literal|"'schemaName' property is deprecated. Use 'schema' instead"
 argument_list|)
 expr_stmt|;
-throw|throw
-operator|new
-name|MojoExecutionException
-argument_list|(
-name|message
-argument_list|,
-name|th
-argument_list|)
-throw|;
 block|}
+return|return
+name|schema
+operator|!=
+literal|null
+condition|?
+name|schema
+else|:
+name|schemaName
+return|;
 block|}
 specifier|final
 class|class
@@ -877,7 +909,8 @@ argument_list|(
 name|ent
 argument_list|)
 expr_stmt|;
-name|logger
+name|getLog
+argument_list|()
 operator|.
 name|info
 argument_list|(
@@ -906,7 +939,8 @@ argument_list|(
 name|ent
 argument_list|)
 expr_stmt|;
-name|logger
+name|getLog
+argument_list|()
 operator|.
 name|info
 argument_list|(
@@ -935,7 +969,8 @@ argument_list|(
 name|ent
 argument_list|)
 expr_stmt|;
-name|logger
+name|getLog
+argument_list|()
 operator|.
 name|info
 argument_list|(
@@ -964,7 +999,8 @@ argument_list|(
 name|ent
 argument_list|)
 expr_stmt|;
-name|logger
+name|getLog
+argument_list|()
 operator|.
 name|info
 argument_list|(
