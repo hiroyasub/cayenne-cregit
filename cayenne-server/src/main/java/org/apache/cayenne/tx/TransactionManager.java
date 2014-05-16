abd14290d220567aac8a94657a152c7546daef87
aadamchik <aadamchik@apache.org>
2014-05-16 18:44:47 -0400

    unused import
    Former-commit-id: 78af1b4687705b87d1192a60a3af01b79ab41f71

--- cayenne-server/src/main/java/org/apache/cayenne/tx/TransactionManager.java
+++ cayenne-server/src/main/java/org/apache/cayenne/tx/TransactionManager.java
@@ -15,20 +15,6 @@ name|tx
 package|;
 end_package
 
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|cayenne
-operator|.
-name|configuration
-operator|.
-name|CayenneRuntime
-import|;
-end_import
-
 begin_comment
 comment|/**  * An optional utility service that simplifies wrapping multiple operations in  * transactions. Users only rarely need to invoke it directly, as all standard  * Cayenne operations are managing their own transactions internally.  *   * @since 3.2  */
 end_comment
