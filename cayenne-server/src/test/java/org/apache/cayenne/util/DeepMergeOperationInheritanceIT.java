cb67d7acb79251c86fe3b74867baa72af8853a04
Savva Kolbechev <s.kolbachev@gmail.com>
2014-11-19 15:44:11 +0300

    fix DBCleaner for Derby
    Former-commit-id: df3494b6b57adf5c92dd4c061e8c243f0d7c588d

--- cayenne-server/src/test/java/org/apache/cayenne/util/DeepMergeOperationInheritanceIT.java
+++ cayenne-server/src/test/java/org/apache/cayenne/util/DeepMergeOperationInheritanceIT.java
@@ -294,6 +294,16 @@ name|DataChannelInterceptor
 name|queryInterceptor
 decl_stmt|;
 annotation|@
+name|Override
+specifier|protected
+name|void
+name|setUpAfterInjection
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+block|}
+annotation|@
 name|Test
 specifier|public
 name|void
