6ea1bee4ff510f8ceb20c3fbb1f220b95ae476fe
ollybondareva <ollybondareva@gmail.com>
2016-10-19 17:37:45 +0300

    Revert to the old Reverse Engineering UI style
    Former-commit-id: 29671e75b38620811c2bfc6c0f2b0af01d4942dd

--- cayenne-server/src/main/java/org/apache/cayenne/configuration/BaseConfigurationNodeVisitor.java
+++ cayenne-server/src/main/java/org/apache/cayenne/configuration/BaseConfigurationNodeVisitor.java
@@ -23,20 +23,6 @@ name|apache
 operator|.
 name|cayenne
 operator|.
-name|dbimport
-operator|.
-name|ReverseEngineering
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|cayenne
-operator|.
 name|map
 operator|.
 name|DataMap
@@ -436,22 +422,6 @@ operator|new
 name|UnsupportedOperationException
 argument_list|(
 literal|"Not implemented for Query"
-argument_list|)
-throw|;
-block|}
-specifier|public
-name|T
-name|visitReverseEngineering
-parameter_list|(
-name|ReverseEngineering
-name|reverseEngineering
-parameter_list|)
-block|{
-throw|throw
-operator|new
-name|UnsupportedOperationException
-argument_list|(
-literal|"Not implemented for ReverseEngineering"
 argument_list|)
 throw|;
 block|}
