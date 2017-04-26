84780d34f2ba8ec6b93fbc4474059edf792d5f3a
Nikita Timofeev <stariy95@gmail.com>
2017-04-26 12:57:37 +0300

    Cleanup: missing license, missing artifact in assemblies
    Former-commit-id: a8960c55971cb349d10bf41f37a2172ab3cdbd9d

--- modeler/cayenne-modeler/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ modeler/cayenne-modeler/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*****************************************************************  *   Licensed to the Apache Software Foundation (ASF) under one  *  or more contributor license agreements.  See the NOTICE file  *  distributed with this work for additional information  *  regarding copyright ownership.  The ASF licenses this file  *  to you under the Apache License, Version 2.0 (the  *  "License"); you may not use this file except in compliance  *  with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing,  *  software distributed under the License is distributed on an  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  *  KIND, either express or implied.  See the License for the  *  specific language governing permissions and limitations  *  under the License.  ****************************************************************/
+end_comment
+
 begin_package
 package|package
 name|org
@@ -48,7 +52,7 @@ import|;
 end_import
 
 begin_comment
-comment|/* *   StaticLoggerBinder bind and replace standard SLF4J LoggerFactory to ModelerLogFactory. *   This is used because in Cayenne Modeler we use custom logger implementation (ModelerLogger) *   for logging in System.err and modeler logger. * */
+comment|/**  * StaticLoggerBinder bind and replace standard SLF4J LoggerFactory to ModelerLogFactory.  * This is used because in Cayenne Modeler we use custom logger implementation (ModelerLogger)  * for logging in System.err and modeler logger.  *  * @since 4.0  */
 end_comment
 
 begin_class
@@ -62,10 +66,23 @@ specifier|private
 specifier|final
 specifier|static
 name|StaticLoggerBinder
-name|singleton
+name|SINGLETON
 init|=
 operator|new
 name|StaticLoggerBinder
+argument_list|()
+decl_stmt|;
+specifier|private
+specifier|final
+specifier|static
+name|String
+name|LOGGER_FACTORY_CLASS_STR
+init|=
+name|ModelerLogFactory
+operator|.
+name|class
+operator|.
+name|getName
 argument_list|()
 decl_stmt|;
 specifier|private
@@ -77,19 +94,6 @@ operator|new
 name|ModelerLogFactory
 argument_list|()
 decl_stmt|;
-specifier|private
-specifier|static
-specifier|final
-name|String
-name|loggerFactoryClassStr
-init|=
-name|ModelerLogFactory
-operator|.
-name|class
-operator|.
-name|getName
-argument_list|()
-decl_stmt|;
 specifier|public
 specifier|static
 name|StaticLoggerBinder
@@ -97,7 +101,7 @@ name|getSingleton
 parameter_list|()
 block|{
 return|return
-name|singleton
+name|SINGLETON
 return|;
 block|}
 specifier|public
@@ -117,9 +121,7 @@ name|getLoggerFactoryClassStr
 parameter_list|()
 block|{
 return|return
-name|this
-operator|.
-name|loggerFactoryClassStr
+name|LOGGER_FACTORY_CLASS_STR
 return|;
 block|}
 block|}
