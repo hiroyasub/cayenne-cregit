8855d992a052bc87a52570590473224be3355f03
Nikita Timofeev <stariy95@gmail.com>
2017-08-01 17:02:33 +0300

    CAY-2340 Save additional metadata as separate files
    Former-commit-id: 3939efe42ab7fd8a3745a45e0d6721b9b789e519

--- cayenne-project/src/main/java/org/apache/cayenne/project/extension/ProjectExtension.java
+++ cayenne-project/src/main/java/org/apache/cayenne/project/extension/ProjectExtension.java
@@ -17,6 +17,20 @@ name|extension
 package|;
 end_package
 
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|cayenne
+operator|.
+name|configuration
+operator|.
+name|ConfigurationNodeVisitor
+import|;
+end_import
+
 begin_comment
 comment|/**  *<p>DataMap XML file extension mechanics.</p>  *<p>  *     Can be used to enhance datamap.map.xml files with additional (really random) information.  *     By default extensions not used by {@link org.apache.cayenne.configuration.server.ServerRuntime} or  *     ClientRuntime so they can safely store big chunks of data.  *</p>  *<p>  *     Extensions can be contributed by {@link org.apache.cayenne.project.ProjectModule#contributeExtensions(org.apache.cayenne.di.Binder)}.  *     {@link org.apache.cayenne.project.ProjectModule} currently used by Modeler and cli tools, e.g. cdbimport and cgen.  *</p>  *  * @since 4.1  */
 end_comment
@@ -34,6 +48,13 @@ function_decl|;
 comment|/**      * @return delegate that handle saving phase of XML processing      */
 name|SaverDelegate
 name|createSaverDelegate
+parameter_list|()
+function_decl|;
+name|ConfigurationNodeVisitor
+argument_list|<
+name|String
+argument_list|>
+name|createNamingDelegate
 parameter_list|()
 function_decl|;
 block|}
