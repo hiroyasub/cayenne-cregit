581c5701f654b338852631a4210a00bc6c399c35
Arseni Bulatski <ancarseni@gmail.com>
2018-11-15 09:32:17 +0300

    Adding dbImport tab to project tab
    Former-commit-id: 2ee1782cad651e355d2653030a47a4e01b6d00b7

--- modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/editor/TabPanel.java
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/editor/TabPanel.java
@@ -1,8 +1,4 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
-begin_comment
-comment|/*****************************************************************  *   Licensed to the Apache Software Foundation (ASF) under one  *  or more contributor license agreements.  See the NOTICE file  *  distributed with this work for additional information  *  regarding copyright ownership.  The ASF licenses this file  *  to you under the Apache License, Version 2.0 (the  *  "License"); you may not use this file except in compliance  *  with the License.  You may obtain a copy of the License at  *  *    http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing,  *  software distributed under the License is distributed on an  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  *  KIND, either express or implied.  See the License for the  *  specific language governing permissions and limitations  *  under the License.  ****************************************************************/
-end_comment
-
 begin_package
 package|package
 name|org
@@ -14,10 +10,6 @@ operator|.
 name|modeler
 operator|.
 name|editor
-operator|.
-name|cgen
-operator|.
-name|domain
 package|;
 end_package
 
@@ -129,37 +121,7 @@ name|javax
 operator|.
 name|swing
 operator|.
-name|JButton
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|swing
-operator|.
-name|JCheckBox
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|swing
-operator|.
-name|JLabel
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|swing
-operator|.
-name|JPanel
+name|*
 import|;
 end_import
 
@@ -169,18 +131,14 @@ name|java
 operator|.
 name|awt
 operator|.
-name|BorderLayout
+name|*
 import|;
 end_import
-
-begin_comment
-comment|/**  * @since 4.1  */
-end_comment
 
 begin_class
 specifier|public
 class|class
-name|CgenPanel
+name|TabPanel
 extends|extends
 name|JPanel
 block|{
@@ -201,10 +159,13 @@ name|DataMap
 name|dataMap
 decl_stmt|;
 specifier|public
-name|CgenPanel
+name|TabPanel
 parameter_list|(
 name|DataMap
 name|dataMap
+parameter_list|,
+name|String
+name|icon
 parameter_list|)
 block|{
 name|setLayout
@@ -333,7 +294,7 @@ name|ModelerUtil
 operator|.
 name|buildIcon
 argument_list|(
-literal|"icon-datamap.png"
+name|icon
 argument_list|)
 argument_list|)
 expr_stmt|;
