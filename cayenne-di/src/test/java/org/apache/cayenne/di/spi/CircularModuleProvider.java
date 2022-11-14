ccde64f3a5e38d8ce1e8c48aec6f91f9ff36c84a
Nikita Timofeev <stariy95@gmail.com>
2022-11-14 15:19:22 +0300

    Add test for the circularly-dependent Modules
    Former-commit-id: 25a45472c4b305f6366888a0074d4a5701c4227c

---
+++ cayenne-di/src/test/java/org/apache/cayenne/di/spi/CircularModuleProvider.java
@@ -0,0 +1,26 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|cayenne
+operator|.
+name|di
+operator|.
+name|spi
+package|;
+end_package
+
+begin_interface
+specifier|public
+interface|interface
+name|CircularModuleProvider
+extends|extends
+name|ModuleProvider
+block|{ }
+end_interface
+
+end_unit
+
